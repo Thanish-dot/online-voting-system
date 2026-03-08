ee6f6a23d2612d59b609ee85442d3960b825db9e
Mohammed Thanish.A <ma7940652@gmail.com>
2026-03-08 19:51:22 +0530

    Fix set debug to false

--- onlinevoting/settings.py
+++ onlinevoting/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "fallback-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['online-voting-system-zycc.onrender.com', 'localhost', '127.0.0.1']
 
